89d4eec6e161a1e174ae09185145da0de154a108
chameleonlabs2021 <96870965+chameleonlabs2021@users.noreply.github.com>
2021-12-30 19:57:59 +0800

    emp_dept ddl dml scripts

--- emp_dep.sql
+++ emp_dep.sql
@@ -1,145 +1,40 @@
-create table dept(  
-  deptno     number(2,0),  
-  dname      varchar2(14),  
-  loc        varchar2(13),  
-  constraint pk_dept primary key (deptno)  
-)
+DROP TABLE IF EXISTS emp;
 
+CREATE TABLE emp (
+  empno decimal(4,0) NOT NULL,
+  ename varchar(10) default NULL,
+  job varchar(9) default NULL,
+  mgr decimal(4,0) default NULL,
+  hiredate date default NULL,
+  sal decimal(7,2) default NULL,
+  comm decimal(7,2) default NULL,
+  deptno decimal(2,0) default NULL
+);
 
-create table emp(  
-  empno    number(4,0),  
-  ename    varchar2(10),  
-  job      varchar2(9),  
-  mgr      number(4,0),  
-  hiredate date,  
-  sal      number(7,2),  
-  comm     number(7,2),  
-  deptno   number(2,0),  
-  constraint pk_emp primary key (empno),  
-  constraint fk_deptno foreign key (deptno) references dept (deptno)  
-)
+DROP TABLE IF EXISTS dept;
 
+CREATE TABLE dept (
+  deptno decimal(2,0) default NULL,
+  dname varchar(14) default NULL,
+  loc varchar(13) default NULL
+);
 
-insert into DEPT (DEPTNO, DNAME, LOC)
-values(10, 'ACCOUNTING', 'NEW YORK')
+INSERT INTO emp VALUES ('7369','SMITH','CLERK','7902','1980-12-17','800.00',NULL,'20');
+INSERT INTO emp VALUES ('7499','ALLEN','SALESMAN','7698','1981-02-20','1600.00','300.00','30');
+INSERT INTO emp VALUES ('7521','WARD','SALESMAN','7698','1981-02-22','1250.00','500.00','30');
+INSERT INTO emp VALUES ('7566','JONES','MANAGER','7839','1981-04-02','2975.00',NULL,'20');
+INSERT INTO emp VALUES ('7654','MARTIN','SALESMAN','7698','1981-09-28','1250.00','1400.00','30');
+INSERT INTO emp VALUES ('7698','BLAKE','MANAGER','7839','1981-05-01','2850.00',NULL,'30');
+INSERT INTO emp VALUES ('7782','CLARK','MANAGER','7839','1981-06-09','2450.00',NULL,'10');
+INSERT INTO emp VALUES ('7788','SCOTT','ANALYST','7566','1982-12-09','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7839','KING','PRESIDENT',NULL,'1981-11-17','5000.00',NULL,'10');
+INSERT INTO emp VALUES ('7844','TURNER','SALESMAN','7698','1981-09-08','1500.00','0.00','30');
+INSERT INTO emp VALUES ('7876','ADAMS','CLERK','7788','1983-01-12','1100.00',NULL,'20');
+INSERT INTO emp VALUES ('7900','JAMES','CLERK','7698','1981-12-03','950.00',NULL,'30');
+INSERT INTO emp VALUES ('7902','FORD','ANALYST','7566','1981-12-03','3000.00',NULL,'20');
+INSERT INTO emp VALUES ('7934','MILLER','CLERK','7782','1982-01-23','1300.00',NULL,'10');
 
-insert into dept  
-values(20, 'RESEARCH', 'DALLAS')
-
-insert into dept  
-values(30, 'SALES', 'CHICAGO')
-
-insert into dept  
-values(40, 'OPERATIONS', 'BOSTON')
-
-
-insert into emp  
-values(  
- 7839, 'KING', 'PRESIDENT', null,  
- to_date('17-11-1981','dd-mm-yyyy'),  
- 5000, null, 10  
-)
-
-
-insert into emp  
-values(  
- 7698, 'BLAKE', 'MANAGER', 7839,  
- to_date('1-5-1981','dd-mm-yyyy'),  
- 2850, null, 30  
-)
-
-
-insert into emp  
-values(  
- 7782, 'CLARK', 'MANAGER', 7839,  
- to_date('9-6-1981','dd-mm-yyyy'),  
- 2450, null, 10  
-)
-
-insert into emp  
-values(  
- 7566, 'JONES', 'MANAGER', 7839,  
- to_date('2-4-1981','dd-mm-yyyy'),  
- 2975, null, 20  
-)
-
-
-insert into emp  
-values(  
- 7788, 'SCOTT', 'ANALYST', 7566,  
- to_date('13-JUL-87','dd-mm-rr') - 85,  
- 3000, null, 20  
-)
-
-insert into emp  
-values(  
- 7902, 'FORD', 'ANALYST', 7566,  
- to_date('3-12-1981','dd-mm-yyyy'),  
- 3000, null, 20  
-)
-
-insert into emp  
-values(  
- 7369, 'SMITH', 'CLERK', 7902,  
- to_date('17-12-1980','dd-mm-yyyy'),  
- 800, null, 20  
-)
-
-insert into emp  
-values(  
- 7499, 'ALLEN', 'SALESMAN', 7698,  
- to_date('20-2-1981','dd-mm-yyyy'),  
- 1600, 300, 30  
-)
-
-insert into emp  
-values(  
- 7521, 'WARD', 'SALESMAN', 7698,  
- to_date('22-2-1981','dd-mm-yyyy'),  
- 1250, 500, 30  
-)
-
-
-insert into emp  
-values(  
- 7654, 'MARTIN', 'SALESMAN', 7698,  
- to_date('28-9-1981','dd-mm-yyyy'),  
- 1250, 1400, 30  
-)
-
-
-insert into emp  
-values(  
- 7844, 'TURNER', 'SALESMAN', 7698,  
- to_date('8-9-1981','dd-mm-yyyy'),  
- 1500, 0, 30  
-)
-
-
-insert into emp  
-values(  
- 7876, 'ADAMS', 'CLERK', 7788,  
- to_date('13-JUL-87', 'dd-mm-rr') - 51,  
- 1100, null, 20  
-)
-
-
-insert into emp  
-values(  
- 7900, 'JAMES', 'CLERK', 7698,  
- to_date('3-12-1981','dd-mm-yyyy'),  
- 950, null, 30  
-)
-
-
-insert into emp  
-values(  
- 7934, 'MILLER', 'CLERK', 7782,  
- to_date('23-1-1982','dd-mm-yyyy'),  
- 1300, null, 10  
-)
-
-select ename, dname, job, empno, hiredate, loc  
-from emp, dept  
-where emp.deptno = dept.deptno  
-order by ename
+INSERT INTO dept VALUES ('10','ACCOUNTING','NEW YORK');
+INSERT INTO dept VALUES ('20','RESEARCH','DALLAS');
+INSERT INTO dept VALUES ('30','SALES','CHICAGO');
+INSERT INTO dept VALUES ('40','OPERATIONS','BOSTON');
